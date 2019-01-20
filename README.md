803dd93f3d3ef2d8bec23ed21de58b2ec15bcb39
Maxim Tolochko <maksim.tolo@gmail.com>
2019-01-20 16:56:19 +0300

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-## to run test
-`npx truffle test`
+## How to use with Truffle
+
+### Installation
+```bash
+npm install @dragonereum/contracts
+```
+
+### Usage example
+```js
+import contract from 'truffle-contract';
+
+import Getter from '@dragonereum/contracts/dist/Getter.json';
+// or
+import { Getter } from '@dragonereum/contracts';
+
+const getterInstance = contract(Getter);
+
+getterInstance.setProvider(window.ethereum);
+```
+
+## How to run tests
+```bash
+npm run test
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+const Battle = require('./dist/Battle.json');
+const BattleController = require('./dist/BattleController.json');
+const BreedingMarketplace = require('./dist/BreedingMarketplace.json');
+const Core = require('./dist/Core.json');
+const CoreController = require('./dist/CoreController.json');
+const Distribution = require('./dist/Distribution.json');
+const DragonCore = require('./dist/DragonCore.json');
+const DragonCoreHelper = require('./dist/DragonCoreHelper.json');
+const DragonGenetics = require('./dist/DragonGenetics.json');
+const DragonGetter = require('./dist/DragonGetter.json');
+const DragonLeaderboard = require('./dist/DragonLeaderboard.json');
+const DragonMarketplace = require('./dist/DragonMarketplace.json');
+const DragonParams = require('./dist/DragonParams.json');
+const DragonStorage = require('./dist/DragonStorage.json');
+const EggCore = require('./dist/EggCore.json');
+const EggMarketplace = require('./dist/EggMarketplace.json');
+const EggStorage = require('./dist/EggStorage.json');
+const Events = require('./dist/Events.json');
+const Getter = require('./dist/Getter.json');
+const GladiatorBattle = require('./dist/GladiatorBattle.json');
+const GladiatorBattleSpectators = require('./dist/GladiatorBattleSpectators.json');
+const GladiatorBattleSpectatorsStorage = require('./dist/GladiatorBattleSpectatorsStorage.json');
+const GladiatorBattleStorage = require('./dist/GladiatorBattleStorage.json');
+const Gold = require('./dist/Gold.json');
+const GoldMarketplace = require('./dist/GoldMarketplace.json');
+const GoldMarketplaceStorage = require('./dist/GoldMarketplaceStorage.json');
+const MainBase = require('./dist/MainBase.json');
+const MainBattle = require('./dist/MainBattle.json');
+const MainMarket = require('./dist/MainMarket.json');
+const MarketplaceController = require('./dist/MarketplaceController.json');
+const Migrations = require('./dist/Migrations.json');
+const Nest = require('./dist/Nest.json');
+const Random = require('./dist/Random.json');
+const SkillMarketplace = require('./dist/SkillMarketplace.json');
+const Treasury = require('./dist/Treasury.json');
+const UpgradeController = require('./dist/UpgradeController.json');
+const User = require('./dist/User.json');
+
+module.exports = {
+  Battle,
+  BattleController,
+  BreedingMarketplace,
+  Core,
+  CoreController,
+  Distribution,
+  DragonCore,
+  DragonCoreHelper,
+  DragonGenetics,
+  DragonGetter,
+  DragonLeaderboard,
+  DragonMarketplace,
+  DragonParams,
+  DragonStorage,
+  EggCore,
+  EggMarketplace,
+  EggStorage,
+  Events,
+  Getter,
+  GladiatorBattle,
+  GladiatorBattleSpectators,
+  GladiatorBattleSpectatorsStorage,
+  GladiatorBattleStorage,
+  Gold,
+  GoldMarketplace,
+  GoldMarketplaceStorage,
+  MainBase,
+  MainBattle,
+  MainMarket,
+  MarketplaceController,
+  Migrations,
+  Nest,
+  Random,
+  SkillMarketplace,
+  Treasury,
+  UpgradeController,
+  User
+};
