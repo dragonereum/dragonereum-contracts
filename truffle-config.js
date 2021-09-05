64776809fafdaf36d7695bfd4672d907acf7d613
Maksim Tolochko <maksim.tolo@gmail.com>
2021-09-05 21:48:57 +0300

    Test

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,9 @@
 const dotenv = require('dotenv');
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const HDWalletProvider = require('truffle-hdwallet-provider');
 const NonceTrackerSubprovider = require('web3-provider-engine/subproviders/nonce-tracker');
 const Web3 = require('web3');
+// const WebsocketProvider = require('web3-providers-ws');
 
 const { toWei } = Web3.utils;
 
@@ -9,7 +11,13 @@ dotenv.config();
 dotenv.config({ path: '.env.local' });
 
 const getProvider = urlOrProvider => () => {
-  const wallet = new HDWalletProvider(process.env.MNEMONIC, urlOrProvider);
+  // const wallet = new HDWalletProvider(process.env.MNEMONIC, urlOrProvider);
+  const wallet = new HDWalletProvider({
+    mnemonic: {
+      phrase: process.env.MNEMONIC
+    },
+    providerOrUrl: urlOrProvider
+  });
   const nonceTracker = new NonceTrackerSubprovider();
 
   wallet.engine._providers.unshift(nonceTracker);
@@ -19,8 +27,14 @@ const getProvider = urlOrProvider => () => {
 };
 
 function infuraWSProvider(network) {
-    const url = `wss://${network}.infura.io/ws/v3/${process.env.INFURA_PROJECT_ID}`;
+    const url = 'wss://ws-matic-mainnet.chainstacklabs.com';
+    // const url = `wss://${network}.infura.io/ws/v3/${process.env.INFURA_PROJECT_ID}`;
+
+    console.log(url);
+
     return new Web3.providers.WebsocketProvider(url);
+
+    // return new WebsocketProvider(url);
 }
 
 module.exports = {
@@ -34,7 +48,6 @@ module.exports = {
       websockets: false,
       gas: 6700000,
     },
-
     live: {
       provider: getProvider('http://127.0.0.1:8545'),
       network_id: 1,
@@ -64,6 +77,31 @@ module.exports = {
       timeoutBlocks: 200,
       gas: 6700000,
       confirmations: 0,
+    },
+    polygon: {
+      provider: getProvider(infuraWSProvider('wss://ws-matic-mainnet.chainstacklabs.com')),
+      network_id: 137,
+      gasPrice: toWei('1', 'gwei'),
+      websockets: true,
+      // websockets: false,
+      skipDryRun: true,
+      timeoutBlocks: 200,
+      gas: 6700000,
+      confirmations: 0,
+      networkCheckTimeout: 100000,
+    },
+    asd: {
+      // provider: getProvider(infuraWSProvider('polygon-mainnet')),
+      provider: getProvider(infuraWSProvider('wss://ws-matic-mumbai.chainstacklabs.com')),
+      network_id: 80001,
+      gasPrice: toWei('1', 'gwei'),
+      websockets: true,
+      // websockets: false,
+      skipDryRun: true,
+      timeoutBlocks: 200,
+      gas: 6700000,
+      confirmations: 0,
+      // networkCheckTimeout: 2000000,
     },
   },
   compilers: {
